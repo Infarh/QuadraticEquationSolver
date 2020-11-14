cd013fe3efece6a5f2ca046f1451488d825f838c
Infarh <shmachilin@yandex.ru>
2020-11-14 18:14:40 +0300

    Добавьте файлы проекта.

---
+++ QuadraticEquationSolver/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+using System;
+using System.Windows.Input;
+
+namespace QuadraticEquationSolver.Infrastructure.Commands.Base
+{
+    abstract class Command : ICommand
+    {
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested += value;
+        }
+
+        bool ICommand.CanExecute(object? parameter) => CanExecute(parameter);
+
+        void ICommand.Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+                Execute(parameter);
+        }
+        
+        protected virtual bool CanExecute(object? parameter) => true;
+
+        protected abstract void Execute(object? parameter);
+    }
+}
---
+++ QuadraticEquationSolver/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using QuadraticEquationSolver.Infrastructure.Commands.Base;
+
+namespace QuadraticEquationSolver.Infrastructure.Commands
+{
+    class CloseWindowCommand : Command
+    {
+        protected override bool CanExecute(object? parameter) => parameter is Window || App.ActiveWindow != null;
+
+        protected override void Execute(object? parameter) => (parameter as Window ?? App.ActiveWindow)?.Close();
+    }
+}
